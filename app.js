b41ea7d163c02cc52f024eda061dd433c4e57746
Michel <michel_abdad@hotmail.com>
2020-03-24 22:35:01 +0100

    compression

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const port = process.env.PORT || 3000
 const express = require('express')
 
 const app = express()
+const compression = require('compression')
+
 const { routeHandler } = require('./modules/router')
 
 app
     .set('view engine', 'ejs')
     .set('views', 'views')
+    .use(compression())
     .use(express.static('static'))
     .use('/', routeHandler())
 
